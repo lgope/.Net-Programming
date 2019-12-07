be9657cc8fbadca707bb9b827723c84c65d841bc
lgope <ilakshmangope@gmail.com>
2019-12-08 00:52:48 +0600

    Delegates Basic 🗽🚀

---
+++ Basic-CSharp/Delegates/Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate int mydel(int aa);
+        int myfunc(int a) { return a * a; }
+        int myfunc2(int a) { return a + a; }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            mydel d = p.myfunc; System.Console.WriteLine(d(5));
+            d = p.myfunc2; System.Console.WriteLine(d(5));
+            System.Console.Read();
+        }
+    }
+}
