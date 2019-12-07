1f56a90141105041a6fe81aa8ce1ce1d1c94ddd7
lgope <ilakshmangope@gmail.com>
2019-12-08 00:55:41 +0600

    Basic Lambda Expression 🗽▶

---
+++ Basic-CSharp/LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        delegate int mydel(int aa, int bb);
+        static void Main(string[] args)
+        {
+            mydel d = (a, b) => a + 2 * b;
+
+            // in above line, read a,b go to a+b*2 to evaluate
+
+            System.Console.WriteLine(d(2, 3));
+            System.Console.Read();
+        }
+    }
+}
